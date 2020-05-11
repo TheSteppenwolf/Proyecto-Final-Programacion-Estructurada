55bd5e58f8ed1ef3a30003483162f5176bfd2008
Steppenwolf <37122517+TheSteppenwolf@users.noreply.github.com>
2020-05-11 16:04:59 -0500

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_Programación_Estructurada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
