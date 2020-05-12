7a96f72a70bde5ea38558183cdf8ce1b62d5fedd
Steppenwolf <37122517+TheSteppenwolf@users.noreply.github.com>
2020-05-12 17:31:09 -0500

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -10,6 +10,55 @@ namespace Proyecto_Final_Programación_Estructurada
     {
         static void Main(string[] args)
         {
+            #region Variables 
+
+            // Variables que almacenan a todos los estudiantes y determinan los parametros para serlo.
+            string[][] nombresEstudiantes;
+            double[][] notasEstudiantes;
+            int[][] asistenciasEstudiantes;
+            string[][] aprobacionEstudiantes;
+            // Variable que establece la cantidad de estudiantes ingresados.
+            int index = 0;
+
+            // Variable para gestionar menus.
+            int opcion1 = 0;
+
+            #endregion
+
+            #region Constructores
+
+            // Inicializa los jagged array para su uso.
+            InicializarValores(out nombresEstudiantes, out notasEstudiantes, out asistenciasEstudiantes, out aprobacionEstudiantes);
+
+            #endregion
+
+            while (opcion1 != 3)
+            {
+                // Menu inicial de opciones.
+                Console.WriteLine("SEFIAN: Controlador de notas");
+                Console.Write("Escoja la opcion que desea:\n\t1.Gestionar estudiantes.\n\t2.Ver registro.\n\t3.Salir.\nOpcion: "); opcion1 = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();                
+            }
+
+            Console.WriteLine("Presione cualquier tecla para salir..."); Console.ReadKey();
         }
+
+        #region Métodos
+
+        #region Inicialización de valores inciales para casos de prueba y para su uso.
+
+        // Inicializa los valores de que almacenan a todos los esutdiantes para su uso o para resetear el registro.
+        static void InicializarValores(out string[][] p_nombresEstudiantes, out double[][] p_notasEstudiantes, out int[][] p_asistenciaEstudiantes, out string[][] p_aprobacionEstudiantes)
+        {
+            p_nombresEstudiantes = new string[100][];
+            p_notasEstudiantes = new double[100][];
+            p_asistenciaEstudiantes = new int[100][];
+            p_aprobacionEstudiantes = new string[100][];
+        }
+
+        #endregion        
+
+        #endregion
+
     }
 }
