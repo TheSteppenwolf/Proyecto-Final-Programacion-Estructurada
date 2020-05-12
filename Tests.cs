efc6efadc64bb37b0770ca41899a7887e47bb6be
Steppenwolf <37122517+TheSteppenwolf@users.noreply.github.com>
2020-05-12 17:59:40 -0500

    Create Tests.cs

---
+++ Tests.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_Programación_Estructurada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variables 
+
+            // Variables que almacenan a todos los estudiantes y determinan los parametros para serlo.
+            string[][] nombresEstudiantes;
+            double[][] notasEstudiantes;
+            int[][] asistenciasEstudiantes;
+            string[][] aprobacionEstudiantes;
+            // Variable que establece la cantidad de estudiantes ingresados.
+            int index = 0;
+
+            // Variable para gestionar menus.
+            int opcion1 = 0;
+
+            #endregion
+
+            #region Constructores
+
+            // Inicializa los jagged array para su uso.
+            InicializarValores(out nombresEstudiantes, out notasEstudiantes, out asistenciasEstudiantes, out aprobacionEstudiantes);
+
+            #endregion
+
+            
+
+            Console.WriteLine("Presione cualquier tecla para salir..."); Console.ReadKey();
+        }
+
+        #region Métodos
+
+        #region Inicialización de valores inciales para casos de prueba y para su uso.
+
+        // Inicializa los valores de que almacenan a todos los esutdiantes para su uso o para resetear el registro.
+        static void InicializarValores(out string[][] p_nombresEstudiantes, out double[][] p_notasEstudiantes, out int[][] p_asistenciaEstudiantes, out string[][] p_aprobacionEstudiantes)
+        {
+            p_nombresEstudiantes = new string[100][];
+            p_notasEstudiantes = new double[100][];
+            p_asistenciaEstudiantes = new int[100][];
+            p_aprobacionEstudiantes = new string[100][];
+        }
+
+        #endregion        
+
+        #endregion
+
+    }
+}
