c60842d89df25c98a199c27dd3d40cc32a0665b8
Steppenwolf <37122517+TheSteppenwolf@users.noreply.github.com>
2020-05-25 21:51:21 -0500

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -39,7 +39,7 @@ namespace Proyecto_Final_Programación_Estructurada
             // Inicializa los jagged array para su uso.
             InicializarValores(out nombresEstudiantes, out apellidosEstudiantes, out pruebasEstudiantes, out examenesEstudiantes, out asistenciasEstudiantes, out aprobacionEstudiantes);
             // Creación de estudiantes para casos de prueba.
-            InicializarValores(ref index, ref nombresEstudiantes, ref apellidosEstudiantes, ref pruebasEstudiantes, ref examenesEstudiantes, ref asistenciasEstudiantes, ref aprobacionEstudiantes);
+            //InicializarValores(ref index, ref nombresEstudiantes, ref apellidosEstudiantes, ref pruebasEstudiantes, ref examenesEstudiantes, ref asistenciasEstudiantes, ref aprobacionEstudiantes);
 
             #endregion
 
